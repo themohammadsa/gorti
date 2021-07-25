05c37582706f377ec20018f58d34f21e3d261755
themohammadsa <59393936+themohammadsa@users.noreply.github.com>
2021-07-25 15:24:32 +0530

    removed search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
-import logo from "./logo.png";
-import logotext from "./logotext.png";
-import github from "./github.svg";
-import { NavBar } from "../NavBar/NavBar";
+import logo from './logo.png';
+import logotext from './logotext.png';
+import github from './github.svg';
+import { NavBar } from '../NavBar/NavBar';
 
 export const Header = ({ selectedTabToHeader }) => {
   return (
@@ -9,43 +9,41 @@ export const Header = ({ selectedTabToHeader }) => {
       <header className="header ">
         <div className="header-content grid-header">
           <div className="flex-row column-header">
-               
-          <a href="https://gorti.netlify.app/">
-            <img src={logo} className="logo " />
-          </a>
+            <a href="https://gorti.netlify.app/">
+              <img src={logo} className="logo " />
+            </a>
 
-          <a href="https://gorti.netlify.app/">            <img src={logotext} className="hide logo-text pad1rem" /> </a>
+            <a href="https://gorti.netlify.app/">
+              {' '}
+              <img src={logotext} className="hide logo-text pad1rem" />{' '}
+            </a>
             <span className="dropdown">v1.0 </span>
           </div>
-          <input
-            className="search-box column-header hide"
-            placeholder="Search the docs"
-          />
 
           <div className="flex-row column-header ">
             <a href="https://github.com/themohammadsa/gorti">
-              <img className="icon-github pad-right-1rem" src={github} />{" "}
+              <img className="icon-github pad-right-1rem" src={github} />{' '}
             </a>
             <a
               download
               href="https://gorti.netlify.app/styles.css"
               style={{
-                textDecoration: "none"
+                textDecoration: 'none',
               }}
             >
               <button
                 style={{
-                  color: "#1e40af",
-                  border: "2px solid #1E40AF",
-                  marginTop: "0.5rem",
-                  backgroundColor: "white"
+                  color: '#1e40af',
+                  border: '2px solid #1E40AF',
+                  marginTop: '0.5rem',
+                  backgroundColor: 'white',
                 }}
                 class="button hide "
               >
                 Download
-              </button>{" "}
+              </button>{' '}
             </a>
-            <NavBar selectTab={selectedTabToHeader} />{" "}
+            <NavBar selectTab={selectedTabToHeader} />{' '}
           </div>
         </div>
       </header>
